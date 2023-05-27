1e47e00e61a4cab3240c3ecdbe407c7b79a5c84a
Rizwan <rizwankhalid171@gmail.com>
2023-05-27 21:40:32 +0100

    Initial commit

---
+++ src/main/java/com/rizwan/quotesapp/QuotesAppApplication.java
@@ -0,0 +1,13 @@
+package com.rizwan.quotesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesAppApplication.class, args);
+	}
+
+}
