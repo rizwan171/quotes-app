d3fe6b5aa4b6771d7945844e4736a0e979fb73cc
Rizwan <rizwankhalid171@gmail.com>
2023-07-16 16:32:26 +0100

    Updated files and unit tests

--- src/main/java/com/rizwan/quotesapp/QuotesAppApplication.java
+++ src/main/java/com/rizwan/quotesapp/QuotesAppApplication.java
@@ -2,8 +2,10 @@ package com.rizwan.quotesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class QuotesAppApplication {
 
 	public static void main(String[] args) {
