1b582365aca6b7fa9feefd5ade29ad45dc791927
Rizwan <rizwankhalid171@gmail.com>
2023-10-23 21:29:40 +0100

    Added request to save a generated quote

--- src/frontend/src/services/modules/QuouteService.ts
+++ src/frontend/src/services/modules/QuouteService.ts
@@ -1,7 +1,27 @@
-export const getAllUserSavedQuotes = async () => {
+import { SERVER_API_URL } from "@/constants/api_urls";
+import type { GeneratedQuote } from "@/types/GeneratedQuote";
+import type { Quote } from "@/types/Quote";
 
-}
+export const getAllUserSavedQuotes = async () => {};
 
-export const saveGeneratedQuote = async () => {
+export const saveGeneratedQuote = async (generatedQuote: GeneratedQuote): Promise<Quote | null> => {
+  const response = await fetch(SERVER_API_URL, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(generatedQuote)
+  });
 
-}
+  if (response.status !== 200) {
+    if (response.status == 400) {
+      // TODO handle bad request
+    }
+
+    return null;
+  }
+
+  const savedQuote = await response.json();
+  return savedQuote as Quote;
+};
--- src/frontend/src/types/Quote.ts
+++ src/frontend/src/types/Quote.ts
@@ -1,1 +1,13 @@
- 
+export type Quote = {
+  id: string;
+  quoteText: string;
+  author: string;
+  origin: string;
+  creationType: CreationType;
+};
+
+enum CreationType {
+  "MANUAL",
+  "SAVED",
+  "GENERATED"
+}
