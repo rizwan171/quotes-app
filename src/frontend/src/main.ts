2e0b807d0142638ad5557abf0aa0301fb0073477
Rizwan <rizwankhalid171@gmail.com>
2023-10-26 20:41:02 +0100

    Added notifications library

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -2,10 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/css/index.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
-
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
 import { faFloppyDisk, faCircleNotch, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import Notifications from "@kyvg/vue3-notification";
 
 library.add(faFloppyDisk, faCircleNotch, faTriangleExclamation);
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+createApp(App).use(Notifications).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
